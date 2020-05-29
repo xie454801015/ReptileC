2363e1a4b8d7ceb72a5aa8df83b314c7db2838dd
xiejp <454801015@qq.com>
2020-05-29 17:44:17 +0800

    no message

---
+++ ESTest/ThrottleTest/Controllers/ApiTestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ThrottleTest.Controllers
+{
+    /// <summary>
+    /// TESTaPI
+    /// </summary>
+    public class ApiTestController:ApiController
+    {
+        /// <summary>
+        /// test1
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
--- ESTest/ThrottleTest/Controllers/HomeController.cs
+++ ESTest/ThrottleTest/Controllers/HomeController.cs
@@ -7,16 +7,26 @@ using ThrottleTest.Attribute.ThrottleAttribute;
 
 namespace ThrottleTest.Controllers
 {
+    /// <summary>
+    /// 首页
+    /// </summary>
     //[Throttle(QPSLimit.LimitingType.LeakageBucket, 15, 1)]
     [Throttle(QPSLimit.LimitingType.TokenBucket, 15, 1)]
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 首页1
+        /// </summary>
+        /// <returns></returns>
         //[Throttle(QPSLimit.LimitingType.LeakageBucket, 1, 20)]
         public ActionResult Index()
         {
             return View();
         }
-        
+        /// <summary>
+        /// about
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -24,19 +34,31 @@ namespace ThrottleTest.Controllers
             return View();
         }
 
+        /// <summary>
+        /// contact
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
-
+        /// <summary>
+        /// 测试1
+        /// </summary>
+        /// <returns></returns>
+        //[HttpGet]
         public ContentResult Test1()
         {
 
             return Content("11111");
         }
-
+        /// <summary>
+        /// 测试2
+        /// </summary>
+        /// <returns></returns>
+        //[HttpGet]
         public ContentResult Test2()
         {
 
